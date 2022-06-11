bd572c26346c13f81c463a60001ea8b055457913
DGross245 <103501922+DGross245@users.noreply.github.com>
2022-06-11 12:16:40 +0200

    125%

--- libft/libft.h
+++ libft/libft.h
@@ -20,7 +20,7 @@ typedef struct s_list
 	void			*content;
 	struct s_list	*next;
 }					t_list;
-t_list	*ft_lstnew(void *counter);
+t_list	*ft_lstnew(void *content);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
